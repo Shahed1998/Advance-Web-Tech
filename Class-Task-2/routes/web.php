3a017fe74b3442b2ec38f4e5c01fd4c40ff1f30d
Shahed1998 <shahedchowdhury120@gmail.com>
2022-02-01 11:30:33 +0600

    ...

--- Class-Task-2/routes/web.php
+++ Class-Task-2/routes/web.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/login', function(){
-    echo "Login page";
+    return view('welcome');
 });
